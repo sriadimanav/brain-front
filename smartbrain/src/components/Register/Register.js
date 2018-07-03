dc8ae671c3c730e2abd563aea5e9b809e2c115cd
sriadimanav <30376679+sriadimanav@users.noreply.github.com>
2018-07-03 14:08:36 +0530

    changing server

--- smartbrain/src/components/Register/Register.js
+++ smartbrain/src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3001/register', {
+		fetch('https://guarded-headland-71094.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
